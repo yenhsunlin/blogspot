7e89303271d648f521ac491f7ac9c025f84925c8
yenhsunlin <38074446+yenhsunlin@users.noreply.github.com>
2019-04-19 10:26:26 +0800

    Update denoise.py

--- Info-Alg/IA10/libs/denoise.py
+++ Info-Alg/IA10/libs/denoise.py
@@ -14,7 +14,7 @@ class denoise_det:
         lam : lambda for penalty
         cutoff : truncated value
         """
-        # Determing the input is color or grey
+        # Determing the input im is color or grey
         if len(im.shape) == 3: # Color
             self.im = np.pad(im, ((1,1),(1,1),(0,0)), mode = 'constant')
             self.row, self.col, self.ch = self.im.shape
@@ -50,14 +50,22 @@ class denoise_det:
                         # the value with minimum energy.
                         # This one is the correct pixel value statistically and will be
                         # saved to self.denoise_im[r,c,ch]
-                        self.denoise_im[r,c,ch] = np.argmin(energy(self.im[r,c,ch],self.posterior[r-1:r+2,c-1:c+2,ch],self.lam,self.cutoff))
+                        self.denoise_im[r,c,ch] = np.argmin(                                           \
+                                                            energy(self.im[r,c,ch],                    \
+                                                                   self.posterior[r-1:r+2,c-1:c+2,ch], \
+                                                                   self.lam, self.cutoff)              \
+                                                           )
             # Updating the posterior image by the complete self.denoise_im.
             # It will be used for calculating neighborhood state in the next iteration.
             self.posterior = self.denoise_im.copy()            
         else: # If image is grey
             for r in range(1,self.row-1):
                 for c in range(1,self.col-1):
-                    self.denoise_im[r,c] = np.argmin(energy(self.im[r,c],self.posterior[r-1:r+2,c-1:c+2],self.lam,self.cutoff))
+                    self.denoise_im[r,c] = np.argmin(                                        \
+                                                     energy(self.im[r,c],                    \
+                                                            self.posterior[r-1:r+2,c-1:c+2], \
+                                                            self.lam, self.cutoff)           \
+                                                    )
             self.posterior = self.denoise_im.copy()
     
     def status(self):
@@ -107,14 +115,16 @@ class denoise_prob:
                 for r in range(1,self.row-1):
                     for c in range(1,self.col-1):
                         # Calculating the energies for total 256 pixel values
-                        ene = energy(self.im[r,c,ch],self.posterior[r-1:r+2,c-1:c+2,ch],self.lam,self.cutoff)
+                        ene = energy(self.im[r,c,ch], self.posterior[r-1:r+2,c-1:c+2,ch], \
+                                     self.lam, self.cutoff)
                         # We sample the pixel value from the probabilities given by the energy array
                         self.denoise_im[r,c,ch] = pix_sampling(ene)
             self.posterior = self.denoise_im.copy()            
         else:
             for r in range(1,self.row-1):
                 for c in range(1,self.col-1):
-                    ene = energy(self.im[r,c],self.posterior[r-1:r+2,c-1:c+2],self.lam,self.cutoff)
+                    ene = energy(self.im[r,c], self.posterior[r-1:r+2,c-1:c+2], \
+                                 self.lam, self.cutoff)
                     self.denoise_im[r,c] = pix_sampling(ene)
             self.posterior = self.denoise_im.copy()
     
@@ -130,10 +140,10 @@ def energy(center, nei, lam, cutoff, pixels = np.arange(256)):
     Calculating the energies of 256 pixel values for the center pixel in the box, Eq. (7)
     """
     Phi = (pixels-center)**2
-    Psi = lam*(np.clip((pixels-nei[0,1])**2,0,cutoff) +   \
-                np.clip((pixels-nei[1,0])**2,0,cutoff) +  \
-                np.clip((pixels-nei[1,2])**2,0,cutoff) +  \
-                np.clip((pixels-nei[2,1])**2,0,cutoff))
+    Psi = lam*(np.clip((pixels-nei[0,1])**2, 0, cutoff) +   \
+                np.clip((pixels-nei[1,0])**2, 0, cutoff) +  \
+                np.clip((pixels-nei[1,2])**2, 0, cutoff) +  \
+                np.clip((pixels-nei[2,1])**2, 0, cutoff))
     return (Psi+Phi)
 
 
@@ -173,4 +183,4 @@ def denoise_multif(imls):
     """
     im = np.float32(imls)
     clean = np.sum(im,axis=0)/len(im)
-    return np.uint8(clean)
+    return np.uint8(np.clip(clean, 0, 255))
